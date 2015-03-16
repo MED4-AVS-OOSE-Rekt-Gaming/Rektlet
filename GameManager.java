ef7b9d1c18f35fc2a8790ae414f986662ec0c087
Meidon <steffen.star@hotmail.com>
2015-03-16 17:40:40 +0100

    The Game Manager (Template)
    Template for the game manger that's going to keep track of vital
    information in the game application

---
+++ GameManager.java
@@ -0,0 +1,14 @@
+
+public class GameManager {
+protected int score;
+protected int lives;
+
+public void Initialize() {
+	/* Insert Functionality to begin GAME here.... */
+}
+
+public void EndGame() {
+	/* Insert Functionality to end GAME here.... */
+}
+
+}
