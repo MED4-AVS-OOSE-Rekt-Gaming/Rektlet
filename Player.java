e52bbfa7aef037bb3474dde4578cc43373440f9a
Minimacakre <minimacakre@live.dk>
2015-03-20 16:01:29 +0100

    Deleted error made by GitHub
    (Sorry for spamming this :s )

--- Player.java
+++ Player.java
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-class Player extends Unit{
-	
-	@Override
-	public String toString(){
-		return "The Player, who is a "+super.toString();
-	}
-	
-	public void Input(){
-
-	}
-}
-=======
 class Player extends Unit{
 	
 	@Override
