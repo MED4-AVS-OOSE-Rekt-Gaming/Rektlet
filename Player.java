91720e49452b31713c6cccd196857d882322e735
Minimacakre <minimacakre@live.dk>
2015-03-20 15:38:42 +0100

    Added Input() function that should work with slick2D installed

--- Player.java
+++ Player.java
@@ -5,7 +5,22 @@ class Player extends Unit{
 		return "The Player, who is a "+super.toString();
 	}
 	
-	public void Input(){
-
-	}
-}
+	//input code should work with slick2D
+	/*public void Input(){
+		if (input.isKeyPressed(Input.KEY_UP)){
+			Move("up");
+		} else {
+			if (input.isKeyPressed(Input.KEY_LEFT)){
+				Move("left");
+			} else { 
+				if (input.isKeyPressed(Input.KEY_DOWN)){
+					Move("down");
+				} else { 
+					if (input.isKeyPressed(Input.KEY_RIGHT)){
+						Move("right");
+					}
+				}
+			}
+		}
+	}*/
+}//EOF
