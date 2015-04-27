b8be32705e0dc7f7dd10eea33b44974a38c61aca
Meidon <steffen.star@hotmail.com>
2015-04-27 11:17:30 +0200

    Added Resources-Structure
    This file is a slick2d must have for all the resource loading needs

---
+++ src/nat.rectgaming/Resources.java
@@ -0,0 +1,52 @@
+package nat.rectgaming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import nat.rectgaming.states.world.Tile;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
+import org.newdawn.slick.SpriteSheet;
+
+public class Resources {
+
+	private static Map<String, Image> images;
+	private static Map<String, SpriteSheet> sprites;
+	private static Map<String, Sound> sounds;
+	
+	public Resources() {
+		images = new HashMap<String, Image>();
+		sprites = new HashMap<String, SpriteSheet>();
+		sounds = new HashMap<String, Sound>();
+		
+	}
+	
+	public static Image loadImage(String path) throws SlickException {
+		return new Image(path, false, Image.FILTER_NEAREST);
+	}
+	
+	public static SpriteSheet loadSprite(String path, int tw, int th) throws SlickException {
+		return new SpriteSheet(loadImage(path), tw,th);
+	}
+	
+	public static SpriteSheet getSprite(String getter) {
+		return sprites.get(getter);
+	}
+	
+	public static Image getSpriteImage(String getter, int x, int y) {
+		return sprites.get(getter).getSubImage(x, y);
+	}
+	
+	public static Image Image(String getter) {
+		return sprites.get(getter);
+	}
+	
+	public static Image getImage(String getter) {
+		return images.get(getter);
+	}
+	public static Sound getSound(String getter) {
+		return sounds.get(getter);
+	}
+}
