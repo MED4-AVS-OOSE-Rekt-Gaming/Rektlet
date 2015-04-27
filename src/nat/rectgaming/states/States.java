287c4b2cd44c35b32d45469226c4336362ed8972
Meidon <steffen.star@hotmail.com>
2015-04-27 13:28:53 +0200

    IMPORTANT NEW STRUCTURE AND FILES
    VERY IMPORTANT TO NOTICE!!!!!!!

---
+++ src/nat/rectgaming/states/States.java
@@ -0,0 +1,9 @@
+package nat.rectgaming.states;
+
+public class States {
+	public static final int MENU = 0; // Default screen on startup of GameApplication	
+	public static final int GAME = 1; // Game Screen, Renders and updates Game play
+	public static final int END = 2; // Game-Over state, (Life reaches zero)
+	
+
+}
