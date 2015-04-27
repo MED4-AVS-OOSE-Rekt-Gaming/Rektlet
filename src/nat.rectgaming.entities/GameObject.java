301ed4e0fc7ca0bb284a9d6b716f6242969dd012
Minimacakre <minimacakre@live.dk>
2015-04-27 13:21:10 +0200

    Added Sprite and Slick2D implementation
    Added an Image named "Sprite" and imported all libraries needed to make
    Slick2D function.

--- src/nat.rectgaming.entities/GameObject.java
+++ src/nat.rectgaming.entities/GameObject.java
@@ -1,10 +1,31 @@
-public class GameObject {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Color;
+
+public abstract class GameObject {
 	int positionX;
 	int positionY;
-	//sprite
-	void Display(){
-		
+	
+	int width;
+	int height;
+	
+	Image sprite; //Sprite of the object
+	Color color; //Color wrapper
+	
+	public GameObject() { //Constructor
+		init();
 	}
+	
+	public abstract void init();  //initialize method
+	
+	public void render(GameContainer gc, Graphics g) { //render method
+		if(sprite != null) {
+			sprite.draw(positionX,positionY,width,height,color);
+		}
+	}
+	
+	public abstract void update(GameContainer gc, int delta); //update method
 	
 	public String toString(){
 		return "GameObject at position ("+positionX+","+positionY+")";
