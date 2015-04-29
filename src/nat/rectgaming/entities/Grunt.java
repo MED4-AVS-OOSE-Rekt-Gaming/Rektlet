ad3f43136eb28ab8048dc7304be8b6e158c6f38c
RikimaruTsuki <tuan.viet@hotmail.com>
2015-04-29 14:40:18 +0200

    Grunt Movement PNG
    Pictures yes

--- src/nat/rectgaming/entities/Grunt.java
+++ src/nat/rectgaming/entities/Grunt.java
@@ -1,5 +1,10 @@
 package nat.rectgaming.entities;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.SlickException;
+
 public class Grunt extends Unit {
 
 	@Override
@@ -15,6 +20,21 @@ public class Grunt extends Unit {
 		damage = 1;
 		isDead = false;
 		canAct = true;
+		
+		try {
+
+			spriteUp = new SpriteSheet(new Image("res/gruntMoveUp.png"),16,16);
+			moveUp = new Animation(spriteUp, 300);
+			spriteLeft = new SpriteSheet(new Image("res/gruntMoveLeft.png"),16,16);
+			moveLeft = new Animation(spriteLeft, 300);
+			spriteDown = new SpriteSheet(new Image("res/gruntMoveDown.png"),16,16);
+			moveDown = new Animation(spriteDown, 300);
+			spriteRight = new SpriteSheet(new Image("res/gruntMoveRight.png"),16,16);
+			moveRight = new Animation(spriteRight, 300);
+
+		} catch (SlickException e) {
+
+		}
 	}
 	
 	
