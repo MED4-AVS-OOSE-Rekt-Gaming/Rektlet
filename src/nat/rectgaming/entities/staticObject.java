39256c2548dfd24cdc0740ecacf1a5a227d97015
Meidon <steffen.star@hotmail.com>
2015-04-29 15:16:09 +0200

    staticObject Script
    This is the abstract method just like unit for static objects on the
    playing field

---
+++ src/nat/rectgaming/entities/staticObject.java
@@ -0,0 +1,13 @@
+package nat.rectgaming.entities;
+
+public abstract class staticObject extends GameObject {
+
+	public boolean isStaticObject = true;
+	
+	staticObject() {
+		positionX = 0;
+		positionY = 0;
+	}
+	
+	
+}
