6d928382aadb2e3275954f9e5a98ce964e1582de
Meidon <steffen.star@hotmail.com>
2015-04-29 11:06:47 +0200

    BasicGame
    The Projekt now uses BasicGame instead of StateBasedGame

--- src/nat/rectgaming/GameManager.java
+++ src/nat/rectgaming/GameManager.java
@@ -1,48 +1,82 @@
 package nat.rectgaming;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import nat.rectgaming.Window;
+import nat.rectgaming.entities.Player;
+import nat.rectgaming.entities.Unit;
 import nat.rectgaming.states.*;
 import nat.rectgaming.states.world.*;
 
 import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.StateBasedGame;
 
-public class GameManager extends StateBasedGame {
+public class GameManager extends BasicGame {
+	
+	private ArrayList<Unit> entities; 
 	
 	public GameManager(){
-		super ("Rektlet");	
-	}
-
-	public static void main(String[] args) {
-		
-		File f = new File("natives");
-		if(f.exists()) System.setProperty("org.lwjgl.librarypath", f.getAbsolutePath());
-		
-		try {
-			AppGameContainer game = new AppGameContainer(new GameManager());
-		game.setDisplayMode(Window.WIDTH, Window.HEIGHT, false); //Missing Window
-		game.start();
-		} catch (SlickException e) {
-			e.printStackTrace();
-		}
-		
+	super ("Rektlet");	
 	}
 	
-	@Override
-	public void initStatesList(GameContainer gc) throws SlickException {
-		gc.setMaximumLogicUpdateInterval(60);
-		gc.setTargetFrameRate(60);
-		gc.setAlwaysRender(true);
-		gc.setShowFPS(false);
-		gc.setVSync(true);
-		
-		new Resources();
-		
-		this.addState(new MenuState());
-		this.addState(new GameState());
+
+public static void main(String[] args) {
+	
+	File f = new File("natives");
+	if(f.exists()) System.setProperty("org.lwjgl.librarypath", f.getAbsolutePath());
+	
+	try {
+		AppGameContainer game = new AppGameContainer(new GameManager());
+	game.setDisplayMode(Window.WIDTH, Window.HEIGHT, false); //Missing Window
+	game.start();
+	} catch (SlickException e) {
+		e.printStackTrace();
 	}
-} //EOF
+}
+
+
+@Override
+public void render(GameContainer gc, Graphics g) throws SlickException {
+	
+	int amount = entities.size();
+	for(int i = 0; i <amount; i++ ) {
+		entities.get(i).render(gc, g);
+	}
+
+}
+
+
+@Override
+public void init(GameContainer gc) throws SlickException {
+	// TODO Auto-generated method stub
+	gc.setMaximumLogicUpdateInterval(60);
+	gc.setTargetFrameRate(60);
+	gc.setAlwaysRender(true);
+	gc.setShowFPS(false);
+	gc.setVSync(true);
+	
+	new Resources();
+	
+	//this.addState(new MenuState());
+	//this.addState(new GameState());
+	
+	entities = new ArrayList<Unit>();
+	
+	entities.add(new Player());
+	entities.get(0).init();
+	
+}	
+
+
+@Override
+public void update(GameContainer gc, int delta) throws SlickException {
+
+	entities.get(0).update(gc, delta);
+
+}
+
+}
