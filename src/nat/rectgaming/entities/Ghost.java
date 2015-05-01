df465923b40dd1dea43c76eed7334907eb4ef9a8
RikimaruTsuki <tuan.viet@hotmail.com>
2015-05-01 14:25:57 +0200

    Fixed sprite on Ghost and Grunt

--- src/nat/rectgaming/entities/Ghost.java
+++ src/nat/rectgaming/entities/Ghost.java
@@ -24,28 +24,28 @@ public class Ghost extends Unit {
 		
 		try { //Override Move
 
-			spriteUp = new SpriteSheet(new Image("res/ghostMoveUp.png"),16,16);
+			spriteUp = new SpriteSheet(new Image("res/images/units/ghost/ghostMoveUp.png"),16,16);
 			moveUp = new Animation(spriteUp, 300);
 			
-			spriteLeft = new SpriteSheet(new Image("res/ghostMoveLeft.png"),16,16);
+			spriteLeft = new SpriteSheet(new Image("res/images/units/ghost/ghostMoveLeft.png"),16,16);
 			moveLeft = new Animation(spriteLeft, 300);
 			
-			spriteDown = new SpriteSheet(new Image("res/ghostMoveDown.png"),16,16);
+			spriteDown = new SpriteSheet(new Image("res/images/units/ghost/ghostMoveDown.png"),16,16);
 			moveDown = new Animation(spriteDown, 300);
 			
-			spriteRight = new SpriteSheet(new Image("res/ghostMoveRight.png"),16,16);
+			spriteRight = new SpriteSheet(new Image("res/images/units/ghost/ghostMoveRight.png"),16,16);
 			moveRight = new Animation(spriteRight, 300);
 			
-			spriteRightUp = new SpriteSheet(new Image("res/ghostMoveRightUp.png"),16,16);
+			spriteRightUp = new SpriteSheet(new Image("res/images/units/ghost/ghostMoveRightUp.png"),16,16);
 			moveRight = new Animation(spriteRight, 300);
 			
-			spriteLeftUp = new SpriteSheet(new Image("res/ghostMoveLeftUp.png"),16,16);
+			spriteLeftUp = new SpriteSheet(new Image("res/images/units/ghost/ghostMoveLeftUp.png"),16,16);
 			moveRight = new Animation(spriteRight, 300);
 			
-			spriteRightDown = new SpriteSheet(new Image("res/ghostMoveRightDown.png"),16,16);
+			spriteRightDown = new SpriteSheet(new Image("res/images/units/ghost/ghostMoveRightDown.png"),16,16);
 			moveRight = new Animation(spriteRight, 300);
 			
-			spriteLeftDown = new SpriteSheet(new Image("res/ghostMoveLeftDown.png"),16,16);
+			spriteLeftDown = new SpriteSheet(new Image("res/images/units/ghost/ghostMoveLeftDown.png"),16,16);
 			moveRight = new Animation(spriteRight, 300);
 
 		} catch (SlickException e) {
