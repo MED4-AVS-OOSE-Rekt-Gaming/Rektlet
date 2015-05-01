29c60a0323c9b68acc6926afa975b56a758abadd
Minimacakre <minimacakre@live.dk>
2015-05-01 14:15:16 +0200

    Updated Movement
    Movement now supports 8 directions, and is no longer stuck to a grid.

--- src/nat/rectgaming/GameManager.java
+++ src/nat/rectgaming/GameManager.java
@@ -17,7 +17,6 @@ import org.newdawn.slick.SlickException;
 
 public class GameManager extends BasicGame {
 	
-	public int GameState = 0;
 	private ArrayList<Unit> entities; 
 	
 	public GameManager(){
@@ -25,105 +24,109 @@ public class GameManager extends BasicGame {
 	}
 	
 
-public static void main(String[] args) {
-	
-	File f = new File("natives");
-	if(f.exists()) System.setProperty("org.lwjgl.librarypath", f.getAbsolutePath());
-	
-	try {
-		AppGameContainer game = new AppGameContainer(new GameManager());
-	game.setDisplayMode(Window.WIDTH, Window.HEIGHT, false);
-	game.start();
-	} catch (SlickException e) {
-		e.printStackTrace();
-	}
-}
-
-
-@Override
-public void render(GameContainer gc, Graphics g) throws SlickException {
-
-	Resources.getImage("testMap").draw();
-	
-	
-	for(int entity = 0; entity < entities.size(); entity++){
-		Unit currEntity = entities.get(entity);
+	public static void main(String[] args) {
 		
-		switch(currEntity.facingDirection){
-			case "up":
-				currEntity.moveUp.draw(currEntity.positionX*16,currEntity.positionY*16);
-				break;
-			case "left":
-				currEntity.moveLeft.draw(currEntity.positionX*16,currEntity.positionY*16);
-				break;
-			case "down":
-				currEntity.moveDown.draw(currEntity.positionX*16,currEntity.positionY*16);
-				break;
-			case "right":
-				currEntity.moveRight.draw(currEntity.positionX*16,currEntity.positionY*16);
-				break;
+		File f = new File("natives");
+		if(f.exists()) System.setProperty("org.lwjgl.librarypath", f.getAbsolutePath());
+		
+		try {
+			AppGameContainer game = new AppGameContainer(new GameManager());
+		game.setDisplayMode(Window.WIDTH, Window.HEIGHT, false);
+		game.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+	
+		Resources.getImage("testMap").draw();
+		
+		
+		for(int entity = 0; entity < entities.size(); entity++){
+			Unit currEntity = entities.get(entity);
+			
+			switch(currEntity.facingDirection){
+				case "up":
+					currEntity.moveUp.draw(currEntity.positionX,currEntity.positionY);
+					break;
+				case "left":
+					currEntity.moveLeft.draw(currEntity.positionX,currEntity.positionY);
+					break;
+				case "down":
+					currEntity.moveDown.draw(currEntity.positionX,currEntity.positionY);
+					break;
+				case "right":
+					currEntity.moveRight.draw(currEntity.positionX,currEntity.positionY);
+					break;
+			}
+		}
+	}
+	
+	
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+	
+		gc.setMaximumLogicUpdateInterval(60);
+		gc.setTargetFrameRate(60);
+		gc.setAlwaysRender(true);
+		gc.setShowFPS(false);
+		gc.setVSync(true);
+		
+		new Resources();
+		
+		entities = new ArrayList<Unit>();
+		
+		entities.add(0, new Player());
+	}	
+
+
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+	
+		//Read player input
+		Input playerInput = gc.getInput();
+		
+		if(playerInput.isKeyDown(Input.KEY_UP) && playerInput.isKeyDown(Input.KEY_LEFT)){
+			entities.get(0).Move("leftUp");
+			System.out.println(entities.get(0).toString());
 		}
 		
-	} if (GameState == 1) {
-	//g.drawRect(50, 50, 100, 100);
-	g.drawString("This is a Test Menu", 50, 50);
-	g.drawString("Press Enter to return to the Game", 50, 150);
-}
-	
-}
-
-
-@Override
-public void init(GameContainer gc) throws SlickException {
-
-	gc.setMaximumLogicUpdateInterval(60);
-	gc.setTargetFrameRate(60);
-	gc.setAlwaysRender(true);
-	gc.setShowFPS(false);
-	gc.setVSync(true);
-	
-	new Resources();
-	
-	entities = new ArrayList<Unit>();
-	
-	entities.add(0, new Player());
-}	
-
-
-@Override
-public void update(GameContainer gc, int delta) throws SlickException {
-if(GameState == 0) {
-	//Read player input
-	Input playerInput = gc.getInput();
-	if(playerInput.isKeyPressed(Input.KEY_UP)){
-		System.out.println("Up was pressed!");
-		entities.get(0).Move("up");
-		System.out.println(entities.get(0).toString());
-	} if(playerInput.isKeyPressed(Input.KEY_LEFT)){
-		System.out.println("Left was pressed!");
-		entities.get(0).Move("left");
-		System.out.println(entities.get(0).toString());
-		//System.out.println();
-	} if(playerInput.isKeyPressed(Input.KEY_DOWN)){
-		System.out.println("Down was pressed!");
-		entities.get(0).Move("down");
-		System.out.println(entities.get(0).toString());
-	} if(playerInput.isKeyPressed(Input.KEY_RIGHT)){
-		System.out.println("Right was pressed!");
-		entities.get(0).Move("right");
-		System.out.println(entities.get(0).toString());
-	}
-}
-	if(gc.getInput().isKeyPressed(Input.KEY_RETURN)) {
-		System.out.println("Loading Menu");
-		GameState = 1;
+		if(playerInput.isKeyDown(Input.KEY_UP) && playerInput.isKeyDown(Input.KEY_RIGHT)){ 
+			entities.get(0).Move("rightUp");
+			System.out.println(entities.get(0).toString());
+		}
 		
+		if(playerInput.isKeyDown(Input.KEY_DOWN) && playerInput.isKeyDown(Input.KEY_LEFT)){
+			entities.get(0).Move("leftDown");
+			System.out.println(entities.get(0).toString());
+		}
+		
+		if(playerInput.isKeyDown(Input.KEY_DOWN) && playerInput.isKeyDown(Input.KEY_RIGHT)){
+			entities.get(0).Move("rightDown");
+			System.out.println(entities.get(0).toString());
+		}
+		
+		if(playerInput.isKeyDown(Input.KEY_UP) && (!playerInput.isKeyDown(Input.KEY_LEFT) && !playerInput.isKeyDown(Input.KEY_RIGHT) && !playerInput.isKeyDown(Input.KEY_DOWN))){
+			entities.get(0).Move("up");
+			System.out.println(entities.get(0).toString());
+		} 
+		
+		if(playerInput.isKeyDown(Input.KEY_LEFT) && (!playerInput.isKeyDown(Input.KEY_UP) && !playerInput.isKeyDown(Input.KEY_RIGHT) && !playerInput.isKeyDown(Input.KEY_DOWN))){
+			entities.get(0).Move("left");
+			System.out.println(entities.get(0).toString());	
+		}
+		
+		if(playerInput.isKeyDown(Input.KEY_DOWN) && (!playerInput.isKeyDown(Input.KEY_UP) && !playerInput.isKeyDown(Input.KEY_RIGHT) && !playerInput.isKeyDown(Input.KEY_LEFT))){
+			entities.get(0).Move("down");
+			System.out.println(entities.get(0).toString());
+		} 
+		
+		if(playerInput.isKeyDown(Input.KEY_RIGHT) && (!playerInput.isKeyDown(Input.KEY_UP) && !playerInput.isKeyDown(Input.KEY_LEFT) && !playerInput.isKeyDown(Input.KEY_DOWN))){
+			entities.get(0).Move("right");
+			System.out.println(entities.get(0).toString());
+		}
 	}
-	if(gc.getInput().isKeyPressed(Input.KEY_RSHIFT)) {
-		System.out.println("Loading Game");
-		GameState = 0;
-	}
-	
-}
-
-}
+} //EOF
