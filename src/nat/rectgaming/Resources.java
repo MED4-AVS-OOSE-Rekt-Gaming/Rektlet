c6c7410edc80088b7d9ee3f888a09b5e3c1dac62
Meidon <steffen.star@hotmail.com>
2015-05-08 13:41:02 +0200

    Changed Resources
    now Resources only contains the functions we actually use

--- src/nat/rectgaming/Resources.java
+++ src/nat/rectgaming/Resources.java
@@ -5,19 +5,15 @@ import java.util.Map;
 
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.Sound;
 import org.newdawn.slick.SpriteSheet;
 
 public class Resources {
 
 	private static Map<String, Image> images;
-	private static Map<String, SpriteSheet> sprites;
-	private static Map<String, Sound> sounds;
-	
+
+
 	public Resources() {
 		images = new HashMap<String, Image>();
-		sprites = new HashMap<String, SpriteSheet>();
-		sounds = new HashMap<String, Sound>();
 		
 		try {
 			images.put("lvl0", loadImage("res/images/levels/lvl0.png"));
@@ -26,33 +22,20 @@ public class Resources {
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public static Image loadImage(String path) throws SlickException {
 		return new Image(path, false, Image.FILTER_NEAREST);
 	}
-	
-	public static SpriteSheet loadSprite(String path, int tw, int th) throws SlickException {
-		return new SpriteSheet(loadImage(path), tw,th);
+
+	public static SpriteSheet loadSprite(String path, int tw, int th)
+			throws SlickException {
+		return new SpriteSheet(loadImage(path), tw, th);
 	}
-	
-	public static SpriteSheet getSprite(String getter) {
-		return sprites.get(getter);
-	}
-	
-	public static Image getSpriteImage(String getter, int x, int y) {
-		return sprites.get(getter).getSubImage(x, y);
-	}
-	
-	public static Image Image(String getter) {
-		return sprites.get(getter);
-	}
-	
+
 	public static Image getImage(String getter) {
 		return images.get(getter);
 	}
-	public static Sound getSound(String getter) {
-		return sounds.get(getter);
-	}
+
 }
